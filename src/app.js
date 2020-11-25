84fcc1487b8742e18f12967580e68101cb7f3ab9
Shuvrajit <shuvrajitdas203@gmail.com>
2020-11-25 07:51:25 +0000

    Added Nodemailer.js in controllers and Mail.js in models

--- src/app.js
+++ src/app.js
@@ -8,6 +8,11 @@ const userRouter = require("./routers/user");
 const app = express();
 const port = process.env.PORT || 3000;
 
+//Creating nodeMailer account
+const nodeMailer = require("./controllers/Nodemailer");
+
+const Mail  = require("./models/Mail");
+
 app.use(express.json());
 app.use(userRouter);
 
@@ -22,6 +27,39 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.post("/contact", function(req, res) {
+	nodeMailer.contact({
+          email: req.body.email,
+          name: req.body.name,
+          message: req.body.message,
+          subject: req.body.subject,
+        });
+	
+	const newMail = new Mail({
+          email: req.body.email,
+          name: req.body.name,
+          message: req.body.message,
+          subject: req.body.subject,
+        });
+	
+		newMail
+          .save()
+          .then((result) => {
+            // console.log(result);
+            res.locals.flashMessages = req.flash(
+              "Your response has been recorded"
+            );
+            res.redirect("/#contact");
+          })
+          .catch((err) => {
+            console.log(err);
+            res.locals.flashMessages = req.flash(
+              "Something went wrong. Please try again"
+            );
+            res.redirect("/#contact");
+          });
+});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
